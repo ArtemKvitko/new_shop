8a0f810736f4a108443c8ede4d5871240327b907
ArtemKvitko <artem.kvitko@loyd.international>
2016-02-25 16:59:10 +0200

    printing info in bucket

--- bucketForm.php
+++ bucketForm.php
@@ -1,1 +1,49 @@
+<form name='retProd' method='post'>
 <?php
+
+if (isset($_POST['retProd'])){
+
+    $rres=$_SESSION['user']->returnAll($_POST['retProd']);
+
+}
+
+
+
+if (!empty( $_SESSION['bucket'])) {
+    $buyed = new Products();
+    foreach($_SESSION['bucket'] as $itm){
+      $item= $buyed->showProduct($itm['product_id'],false);
+      //  echo '<pre>'.var_dump($pr);
+
+     echo"<div id='showName'> <img src='img/icon/".$item->brand.".png' height='24px' id='justLeft'>
+
+                     <h2> ".$item->brand." - </h2> <h2> ".$item->name." </h2> </div><br>
+                     <div id='bucketItems'>
+                     <div id='justLeft'><img src='img/".$item->pic."' id='imgitem' ></div>
+
+                      <div id='specyfication'> <h4>";
+
+
+                $res = json_decode($item->specyfication);
+
+                foreach($res as $k=>$v){
+                    $result[$k]['value'] = $v;
+                    if(empty($v)){
+                        $result[$k]['value']= 'Not available';
+                    }
+                   echo $k.' : '.$result[$k]['value'].'<br>' ;
+                }
+
+                echo '</h4></div>You`ve purchased <b id="err">'.$itm['purchase_count'].'</b> items <br>';
+                echo '<button type="submit" id="button" name="retProd" value="'.$itm['product_id'].'"> Return all </button>';
+
+                 echo   "</div>";
+
+    }
+
+
+} else {
+    echo '<h1>Nothing is added to cart yet </h1>';
+}
+?>
+</form>
